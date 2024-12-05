12b2c1a2174732fa37ae32a0e625f4ed44fa3e24
Deathcoald <94114839+Deathcoald@users.noreply.github.com>
2024-12-05 22:23:33 +0300

    Add files via upload

---
+++ Cinema/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Cinema
+{
+    public partial class Form1 : Form
+    {
+        
+
+        private void OpenFormButton_Click(object sender, EventArgs e)
+        {
+            
+            Form2 form2 = new Form2();
+            form2.Show();
+        }
+
+        public void createText(string name, int size_x, int size_y, int point_x, int point_y, int text_size)
+        {
+            Label cinema_name1 = new Label();
+
+            cinema_name1.Text = name;
+            cinema_name1.Size = new System.Drawing.Size(size_x, size_y);
+            cinema_name1.Location = new System.Drawing.Point(point_x, point_y);
+            cinema_name1.Font = new System.Drawing.Font("Times New Roman", text_size);
+            cinema_name1.ForeColor = System.Drawing.Color.White;
+
+            this.Controls.Add(cinema_name1);
+        }
+
+        private void createButton(string text, int size_x, int size_y, int point_x, int point_y)
+        {
+            Button button = new Button();
+
+            button.Text = text;
+            button.Size = new System.Drawing.Size(size_x, size_y);
+            button.Location = new System.Drawing.Point(point_x, point_y);
+            button.ForeColor = System.Drawing.Color.White;
+
+            button.Click += OpenFormButton_Click; 
+            this.Controls.Add(button);
+        }
+
+        private void createImage(string image_name, int point_x, int point_y)
+        {
+            PictureBox pictureBox = new PictureBox();
+
+            pictureBox.Size = new System.Drawing.Size(400, 600);
+            pictureBox.Location = new System.Drawing.Point(point_x, point_y);
+            pictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox.Image = System.Drawing.Image.FromFile(image_name);
+
+            this.Controls.Add(pictureBox);
+        }
+
+        private void InitializeComponent()
+        {
+            this.BackColor = System.Drawing.Color.Black;
+
+            this.SuspendLayout();
+
+            this.ClientSize = new System.Drawing.Size(1900, 1000);
+            this.Name = "Form1";
+            this.Text = "Афиша";
+
+            this.createText("Добро пожаловать в кино!", 400, 100, 800, 50, 22);
+            this.createText("Марсианин", 210, 80, 430, 160, 20);
+            this.createText("Темный рыцарь", 300, 80, 860, 160, 20);
+            this.createText("Титаник", 300, 80, 1340, 160, 20);
+
+            this.createImage("marsianin.jpg", 300, 250);
+            this.createImage("darkknight.jpg", 750, 250);
+            this.createImage("titanic.jpg", 1200, 250);
+
+            this.createButton("Просмотреть сеансы", 150, 70, 870, 900);
+            this.createButton("Просмотреть сеансы", 150, 70, 430, 900);
+            this.createButton("Просмотреть сеансы", 150, 70, 1330, 900);
+
+            this.ResumeLayout(false);
+        }
+    }
+}
---
+++ Cinema/Form2.Designer.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Cinema
+{
+    public partial class Form2 : Form
+    {
+        static string[] ReadFile(string filePath)
+        {
+            try
+            {
+                return File.ReadAllLines(filePath);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка при чтении файла: " + ex.Message);
+                return new string[] { };
+            }
+        }
+
+        void create_button_text(string line, ref int point_x, ref int point_y)
+        {
+            Label seansLabel = new Label();
+
+            seansLabel.Text = line;
+            seansLabel.Size = new System.Drawing.Size(400, 30);
+            seansLabel.Location = new System.Drawing.Point(point_x, point_y);
+            seansLabel.Font = new System.Drawing.Font("Times New Roman", 20);
+            seansLabel.ForeColor = System.Drawing.Color.White;
+            
+
+            this.Controls.Add(seansLabel);
+
+            point_y += 40;
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+
+            string[] seans_time = ReadFile("timetable.txt");
+
+            int point_x = 500;
+            int point_y = 100;
+
+            foreach (string s in seans_time)
+            {
+                if (string.IsNullOrWhiteSpace(s))
+                {
+                    point_x += 400;
+                    point_y = 100;
+                }
+                else
+                {
+                    this.create_button_text(s, ref point_x, ref point_y);
+                }
+            }
+
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1920, 1000);
+            this.Name = "Form2";
+            this.Text = "Сеансы";
+
+            this.ResumeLayout(false);
+        }
+    }
+}
