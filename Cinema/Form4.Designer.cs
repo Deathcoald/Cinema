01c78654cd56cae47208dd221aa13757c620f16d
Deathcoald <koncevich13@mail.ru>
2024-12-10 11:45:50 +0300

    Final

---
+++ Cinema/Form4.Designer.cs
@@ -0,0 +1,28 @@
+﻿namespace Cinema
+{
+    partial class Form4
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(400, 350);
+            this.Text = "Form4";
+        }
+    }
+}
